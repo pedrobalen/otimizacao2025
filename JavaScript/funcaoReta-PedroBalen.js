d087ead26a4d07e158828969d5b654ed754909f8
Pedro Balen <p.balen99@gmail.com>
2025-03-24 08:18:42 -0300

    Update funcaoReta-PedroBalen.js

--- JavaScript/funcaoReta-PedroBalen.js
+++ JavaScript/funcaoReta-PedroBalen.js
@@ -1,13 +1,19 @@
-const x1 = Math.floor(Math.random() * 4);
-const y1 = 0;
-const x2 = 0;
-const y2 = Math.floor(Math.random() * 4);
+const x1 = 0; 
+const y1 = 3;
+const x2 = 6;
+const y2 = 0;
 
 console.log("x1: ", x1);
+console.log("y1: ", y1);
+console.log("x2: ", x2);
 console.log("y2: ", y2);
 
-const b = x2 + y2;
+
+const a = (y2 - y1) / (x2 - x1);
+console.log("a: ", a);
+
+
+const b = y1 - a * x1;
 console.log("b: ", b);
 
-const a = Math.floor((b + y1) / x1);
-console.log("a: ", a);
+
